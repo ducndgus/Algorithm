b5b8cd12313fc88786a4a3f2fe21bcbdb4e575b5
ducndgus <40850293+ducndgus@users.noreply.github.com>
2022-01-24 21:56:17 +0900

    인텔리제이 테스트

---
+++ algorithm/src/BJ/BJ_3085.java
@@ -0,0 +1,11 @@
+package BJ;
+import java.util.*;
+
+public class BJ_3085 {
+    public static void main(String[] args){
+        int N;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("1");
+
+    }
+}
