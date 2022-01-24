c017a6b59cb2441b54227a4ccfbfa10f05caf348
ducndgus <40850293+ducndgus@users.noreply.github.com>
2022-01-24 22:12:04 +0900

    20220124 오늘은 졸려

---
+++ algorithm/src/BJ/BJ_3085.java
@@ -0,0 +1,20 @@
+package BJ;
+import java.util.*;
+
+public class BJ_3085 {
+	public static void main(String[] args) {
+		int N;
+		String[][] m = new String[50][50];
+		Scanner sc = new Scanner(System.in);
+		N=sc.nextInt();
+		int answer=0;
+		
+		for(int i = 0; i<N; ++i) {
+			for(int j=0; j<N; ++j) {
+				m[i][j]=sc.next();
+			}
+		}
+		
+		
+	}
+}
